1154a85e5047160c6247ff314c0c6b9bd5af7e72
myrandathompson <myrandathompson@icloud.com>
2024-12-08 16:26:24 -0800

    phase 1 and 2 complete

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,6 +2,11 @@
 const express = require('express');
 const router = express.Router();
 
+// backend/routes/index.js
+// ...
+const apiRouter = require('./api');
+
+
 router.get('/hello/world', function(req, res) {
   res.cookie('XSRF-TOKEN', req.csrfToken());
   res.send('Hello World!');
@@ -19,6 +24,9 @@ router.get("/api/csrf/restore", (req, res) => {
     });
   });
   // ...
-
+  
+  router.use('/api', apiRouter);
+  // ...
+  
   
 module.exports = router;
