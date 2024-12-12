c3f8065be7ddc5cc88b95d3f11defb79b5aceccb
myrandathompson <myrandathompson@icloud.com>
2024-12-11 16:19:16 -0800

    Add User logout backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -46,4 +46,17 @@ router.post(
     }
   );
 
+
+
+
+  // Log out
+router.delete(
+    '/',
+    (_req, res) => {
+      res.clearCookie('token');
+      return res.json({ message: 'success' });
+    }
+  );
+
+  
 module.exports = router;
