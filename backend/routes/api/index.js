b4f3700d9167215ca92952f8aa546bb50f5362dd
Myranda Thompson <myrandathompson@icloud.com>
2025-01-04 14:19:56 -0500

    Update index.js

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const sessionRouter = require("./session.js");
-const usersRouter = require("./user.js");
+const usersRouter = require("./users.js");
 const spotsRouter = require("./spots.js");
 const bookingsRouter = require("./bookings.js");
 const reviewsRouter = require("./reviews.js");
