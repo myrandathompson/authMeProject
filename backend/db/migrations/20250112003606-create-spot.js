13223cc0b43c6f1a7dad9ccdcaffe6bf85262151
myrandathompson <myrandathompson@icloud.com>
2025-01-13 09:22:05 -0800

    still working

--- backend/db/migrations/20250112003606-create-spot.js
+++ backend/db/migrations/20250112003606-create-spot.js
@@ -8,7 +8,6 @@ if (process.env.NODE_ENV === 'production') {
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    options.tableName = "Spots";
     await queryInterface.createTable('Spots', {
       id: {
         allowNull: false,
