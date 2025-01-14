91437e95d0c735987ffbce0f4585c94ee31947c0
myrandathompson <myrandathompson@icloud.com>
2025-01-13 23:24:33 -0800

    please work

--- backend/db/seeders/20250111010011-user.js
+++ backend/db/seeders/20250111010011-user.js
@@ -62,7 +62,7 @@ module.exports = {
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
       username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] },
-  },{});
+    });
     /**
      * Add commands to revert seed here.
      *
--- backend/db/seeders/20250111010446-spot.js
+++ backend/db/seeders/20250111010446-spot.js
@@ -7,7 +7,8 @@ if (process.env.NODE_ENV === 'production') {
 }
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('Spots', [
+    options.tableName = 'Spots';
+    await queryInterface.bulkInsert('options', [
       {
         id: 1,
         userId: 1,
@@ -293,7 +294,7 @@ module.exports = {
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options,{
       id: { [Op.in]: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]}
-    }, {});
+    });
 
     
     }  
