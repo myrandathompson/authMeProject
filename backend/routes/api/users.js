e418ee23c54dbc89614e2bb8a9d7d5a2c107ce5b
myrandathompson <myrandathompson@icloud.com>
2025-01-16 11:36:19 -0800

    signup

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -5,6 +5,7 @@ const { User } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
+const bcrypt = require("bcryptjs");
 
 
 // backend/routes/api/users.js
